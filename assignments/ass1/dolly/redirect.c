b8974067e40eae957df3b19e51608048742bab41
Emil Lindholm Brandt <emil93@gmail.com>
2018-11-22 10:08:35 +0100

    dolly done

--- assignments/ass1/dolly/redirect.c
+++ assignments/ass1/dolly/redirect.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
-    printf("Don't get in my way\n");
+    int pid = fork();
+
+    if (pid == 0) {
+        int fd = open("quotes.txt", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+        dup2(fd, 1);
+        close(fd);
+        execl("boba.o", "boba.o", NULL); // WEOW
+        printf("exec failed\n");
+    } else {
+        wait(NULL);
+    }
+
     return 0;
 }
